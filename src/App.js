94a761d6edd552e6a95d600dec99aa440e11a1be
FrancoMendo <francomendo10@gmail.com>
2020-12-18 08:29:49 -0300

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/css/bootstrap.css";
+import { Route, /* Switch */ } from 'react-router-dom';
 
 function App() {
   return (
